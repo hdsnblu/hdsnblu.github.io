3e8ea9fb1c4083c336c5e5f190e7304ded906089
wakeem <134740381+hdsnblu@users.noreply.github.com>
2023-06-08 18:05:42 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+let popup = document.getElementById('popup');
+let equalCount = 0;
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculateResult() {
+  const result = eval(display.value);
+  display.value = result;
+  equalCount++;
+
+  if (equalCount === 3) {
+    popup.style.display = 'block';
+    equalCount = 0;
+  }
+}
+
+function closePopup() {
+  popup.style.display = 'none';
+}
